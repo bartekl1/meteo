3a5ac98d187adc0cb6ed34764c941d76fd00c2c6
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2023-09-10 15:07:55 +0200

    Added database

---
+++ meteo.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Wrz 10, 2023 at 03:05 PM
+-- Wersja serwera: 10.5.19-MariaDB-0+deb11u2
+-- Wersja PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `meteo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `readings`
+--
+
+CREATE TABLE `readings` (
+  `id` int(11) NOT NULL,
+  `read_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `bme280_temperature` float NOT NULL,
+  `bme280_humidity` float NOT NULL,
+  `bme280_pressure` float NOT NULL,
+  `ds18b20_temperature` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `readings`
+--
+ALTER TABLE `readings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `readings`
+--
+ALTER TABLE `readings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
