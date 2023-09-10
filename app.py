4ce1d3284128af3a8a70d4b23f6441d591c71951
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2023-09-10 14:02:28 +0200

    Added current reading to API

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask
 
+import bme280
+import smbus2
+from w1thermsensor import W1ThermSensor
+
 import json
+import os
 
 app = Flask(__name__)
+
+bme280_port = 1
+bme280_address = 0x76
+bme280_bus = smbus2.SMBus(bme280_port)
+
+bme280.load_calibration_params(bme280_bus, bme280_address)
+
+w1_sensor = W1ThermSensor()
 
 
 def load_configs(c):
@@ -26,6 +39,28 @@ with open('configs.json') as file:
 
 with open('version.json') as file:
     current_version = json.load(file)['version']
+
+
+@app.route('/api/current_reading')
+def current_reading_api():
+    bme280_data = bme280.sample(bme280_bus, bme280_address)
+    bme280_humidity = bme280_data.humidity
+    bme280_pressure = bme280_data.pressure
+    bme280_temperature = bme280_data.temperature
+
+    w1_temperature = w1_sensor.get_temperature()
+
+    return {
+        "bme280": {
+            "temperature": bme280_temperature,
+            "humidity": bme280_humidity,
+            "pressure": bme280_pressure
+        },
+        "ds18b20": {
+            "temperature": w1_temperature
+        }
+    }
+
 
 if __name__ == '__main__':
     if 'flask' in configs.keys():
