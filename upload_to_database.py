47c07a32c43955c2e3f48bfbbdadd6428f6b8154
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2024-01-05 19:45:53 +0100

    Add air quality to database upload script

--- upload_to_database.py
+++ upload_to_database.py
@@ -34,19 +34,39 @@ def main():
     r = requests.get(f'http://localhost:{http_port}/api/current_reading')
     rj = r.json()
 
-    bme280_temperature = rj['bme280']['temperature']
-    bme280_humidity = rj['bme280']['humidity']
-    bme280_pressure = rj['bme280']['pressure']
+    try:
+        bme280_temperature = rj['bme280']['temperature']
+        bme280_humidity = rj['bme280']['humidity']
+        bme280_pressure = rj['bme280']['pressure']
+    except Exception:
+        bme280_temperature = None
+        bme280_humidity = None
+        bme280_pressure = None
 
-    ds18b20_temperature = rj['ds18b20']['temperature']
+    try:
+        ds18b20_temperature = rj['ds18b20']['temperature']
+    except Exception:
+        ds18b20_temperature = None
+
+    try:
+        pms5003_pm_1_0 = rj['pms5003']['pm1.0']
+        pms5003_pm_2_5 = rj['pms5003']['pm2.5']
+        pms5003_pm_10 = rj['pms5003']['pm10']
+    except Exception:
+        pms5003_pm_1_0 = None
+        pms5003_pm_2_5 = None
+        pms5003_pm_10 = None
 
     db = mysql.connector.connect(**configs['mysql'])
     cursor = db.cursor()
-    sql = 'INSERT INTO readings (bme280_temperature, bme280_humidity, bme280_pressure, ds18b20_temperature) VALUES (%s, %s, %s, %s)'
+    sql = 'INSERT INTO readings (bme280_temperature, bme280_humidity, bme280_pressure, ds18b20_temperature, pms5003_pm_1_0, pms5003_pm_2_5, pms5003_pm_10) VALUES (%s, %s, %s, %s, %s, %s, %s)'
     cursor.execute(sql, (bme280_temperature,
                          bme280_humidity,
                          bme280_pressure,
-                         ds18b20_temperature))
+                         ds18b20_temperature,
+                         pms5003_pm_1_0,
+                         pms5003_pm_2_5,
+                         pms5003_pm_10))
     db.commit()
     cursor.close()
     db.close()
